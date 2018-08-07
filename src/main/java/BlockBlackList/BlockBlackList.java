16f8907e4c2329adc2e10171656d4fff42343cee
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-08-07 12:51:44 +0300

    Update

--- src/main/java/BlockBlackList/BlockBlackList.java
+++ src/main/java/BlockBlackList/BlockBlackList.java
@@ -1,25 +1,18 @@
 package BlockBlackList;
 
-import java.util.List;
-import java.lang.Object;
 import cn.nukkit.Player;
-import cn.nukkit.Server;
-import cn.nukkit.block.Block;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockPlaceEvent;
-import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.plugin.PluginManager;
-import cn.nukkit.utils.Config;
 import cn.nukkit.utils.TextFormat;
+import java.util.List;
 
 public class BlockBlackList
 extends PluginBase
 implements Listener {
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents((Listener)this, (Plugin)this);
-        this.getLogger().info(TextFormat.GREEN + "BlockBlackList enabled!");
+        this.getServer().getPluginManager().registerEvents(this, this);
         this.saveDefaultConfig();
         this.getConfig();
     }
@@ -28,19 +21,14 @@ implements Listener {
     public void onBlockPlace(BlockPlaceEvent e) {
        List<Integer> listedBlocks = this.getConfig().getIntegerList("Blocks");
        for (Integer block : listedBlocks) {
-       if (e.getBlock().getId() !=block) continue;
-       if (e.getPlayer().isOp()) continue;
-       e.setCancelled(true);
-       this.sendCustomMessage(e.getPlayer());
-    }
-}
+           if (e.getBlock().getId() != block) continue;
+           if (e.getPlayer().isOp()) continue;
+           e.setCancelled(true);
+           this.sendCustomMessage(e.getPlayer());
+       }
+   }
 
     public void sendCustomMessage(Player p) {
-        p.sendMessage(TextFormat.colorize((char)'&', (String)this.getConfig().getString("Message")));
-    }
-
-    @Override
-    public void onDisable() {
-        this.getLogger().info(TextFormat.RED + "BlockBlackList disabled!");
+        p.sendMessage(TextFormat.colorize('&', this.getConfig().getString("Message")));
     }
 }

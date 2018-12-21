7704686ad8f2898aefec8fcdbea66ecd9fb4bfa4
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-12-21 11:33:36 +0200

    Update

--- src/main/java/BlockBlackList/BlockBlackList.java
+++ src/main/java/BlockBlackList/BlockBlackList.java
@@ -6,26 +6,27 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockPlaceEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
-import java.util.List;
 
-public class BlockBlackList
-extends PluginBase
-implements Listener {
+public class BlockBlackList extends PluginBase implements Listener {
+
+    @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
         this.saveDefaultConfig();
-        this.getConfig();
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
-       List<Integer> listedBlocks = this.getConfig().getIntegerList("Blocks");
-       for (Integer block : listedBlocks) {
-           if (e.getBlock().getId() != block) continue;
-           if (e.getPlayer().isOp()) continue;
-           e.setCancelled(true);
-           this.sendCustomMessage(e.getPlayer());
-       }
+        Player p = e.getPlayer();
+        int block = e.getBlock().getId();
+        if (this.getConfig().getIntegerList("Blocks").contains(block)) {
+            if (p.hasPermission("bbl.bypass") || p.hasPermission("bbl.bypass.id." + block)) {
+                return;
+            }
+
+            e.setCancelled(true);
+            this.sendCustomMessage(p);
+        }
    }
 
     public void sendCustomMessage(Player p) {
